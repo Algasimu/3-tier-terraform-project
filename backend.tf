3cdde64a33f612d7ad537e33dcefbe9fe9bea5e2
algajallow@gmail.com <algajallow@gmail.com>
2025-07-26 01:28:23 -0500

    new code 2

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "state-s3bucket01"
-    key    = "state"
+    key    = "env/dev/terraform.tfstate"
     region = "us-east-2"
     dynamodb_table = "state-terraform-db"
   }
